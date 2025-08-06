cde5d917b92f66c7230a87dba7f0f4579e56eb0f
Achyutkulkarni-rgb <achyutkulkarni105@gmail.com>
2025-08-06 23:12:56 +0530

    Fix: resolved Chat.js integration issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // ✅ Your imports
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import ChatPage from './chat';
+import ChatPage from "./chat";
+
 
 const loginPageStyle = {
   minHeight: '100vh',
